96142ca3219782ce69344e0628e752bad7c315f0
francoromeo <francoromeo5012@gmail.com>
2023-12-24 23:01:42 -0300

    status

--- js/script.js
+++ js/script.js
@@ -10,11 +10,27 @@ function bgAnimationItems(){
 }
 bgAnimationItems();
 
+const filterBtnsContainer = document.querySelector(".portfolio-filter");
+filterBtnsContainer.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("portfolio-filter-btn") && 
+    !e.target.classList.contains("active")){
+        filterBtnsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        filterItems(e.target);
+    }
+})
+
 function filterItems(filterBtn){
     const selectCategory = filterBtn.getAttribute("data-filter");
     document.querySelectorAll(".portfolio-item").forEach((item) => {
         document.querySelectorAll(".portfolio-item").forEach((item) =>{
             const category = item.getAttribute('data-category').split(',');
+            if(category.indexOf(selectCategory) !== -1 || selectCategory === "all"){
+                item.classList.add("show");
+            }
+            else{
+                item.classList.remove("show");
+            }
         })
     })
 }
